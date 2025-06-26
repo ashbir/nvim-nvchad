08f52c442a96e33fda0407bda808451475d0953a
Ashbir Aviat Fadila <fadila@ashbir.com>
2025-06-26 13:17:17 +0900

    nvim: add OSC 52 supports to nvchad

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,24 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
+
+vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
